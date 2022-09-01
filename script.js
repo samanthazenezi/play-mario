bba4edfa85fb6137bc828ad49c6273341c5167ee
Samantha Zenezi <samanthazenezi@gmail.com>
2022-09-01 11:28:34 -0300

    index, assets, style e js

---
+++ script.js
@@ -0,0 +1,48 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+
+function jump() {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 1000)
+}
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
+        pipe.style.animation = 'stop';
+        pipe.style.left = 120;
+
+        mario.style.animation = 'none';
+        mario.style.left = 80;
+        mario.src = './assets/game-over.png'
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
+
+        return modalReiniciar();
+
+        clearInterval(loop)
+    }
+}, 10);
+
+function modalReiniciar(){
+    document.querySelector("#modal-reiniciar").classList.toggle("visivel");
+}
+
+function reiniciar(){
+    location.reload();
+}
+
+function iniciar(){
+    location.reload();
+}
+
+
+
+
+document.addEventListener('keydown', jump);
